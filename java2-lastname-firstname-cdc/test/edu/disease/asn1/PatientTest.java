d9c6e094d17cb9f30c27b48db986a93a07a1192e
Anjali123tiwary <125289835+Anjali123tiwary@users.noreply.github.com>
2023-08-22 18:32:30 +0530

    Update PatientTest.java

--- java2-lastname-firstname-cdc/test/edu/disease/asn1/PatientTest.java
+++ java2-lastname-firstname-cdc/test/edu/disease/asn1/PatientTest.java
@@ -2,25 +2,76 @@ package edu.disease.asn1;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.UUID;
+
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 class PatientTest {
-
-	 Exposure exposure;
+private Patient patient;
+	
+	 private static final String FIRST_NAME = "Anjali";
+	 private static final String LAST_NAME = "kumari";
+	 private Exposure exposure;
 	 @Test
-	 public void testSetPatientId()
+		public void setUp() {
+			final UUID uuid = UUID.randomUUID();
+			exposure = new Exposure(uuid);
+//			exposure.setDateTime(LocalDateTime.now());
+			exposure.setExposureType("D");
+			patient = new Patient(1,1);
+			patient.setDiseaseIds(new UUID[] {UUID.randomUUID(), UUID.randomUUID()});
+			patient.setExposures(new Exposure[] {exposure});
+			patient.setFirstName(FIRST_NAME);
+			patient.setLastName(LAST_NAME);
+			patient.setPatientId(uuid);
+			//patient.getFirstName();
+		}
+	 @Test
+		public void testPatientPostiveScenario()
 		{
-		    Patient Test = new Patient(3,3);
-		    Test.setPatientId(UUID.randomUUID());
-		  
+		 final UUID uuid = UUID.randomUUID();
+		 exposure = new Exposure(uuid);
+//			exposure.setDateTime(LocalDateTime.now());
+			exposure.setExposureType("D");
+			patient = new Patient(1,1);
+			patient.setDiseaseIds(new UUID[] {UUID.randomUUID(), UUID.randomUUID()});
+			patient.setExposures(new Exposure[] {exposure});
+			patient.setFirstName(FIRST_NAME);
+			patient.setLastName(LAST_NAME);
+			patient.setPatientId(uuid);
+		 System.out.println(patient.getFirstName());
+			assertNotNull(patient.toString());
+			assertNotNull(patient.getDiseaseIds());
+			assertNotNull(patient.getExposures());
+			assertNotNull(patient.getFirstName());
+			assertNotNull(patient.getLastName());
+//			assertNotNull(patient.hashCode());
+			assertNotNull(patient.getPatientId());
+//			final Patient patientObj = patient;
+//			assertTrue(patient.equals(patientObj));
+//			assertTrue(patient.equals(patient));
+//			assertFalse(patient.equals(null));
+			final Patient patientObjEquals = new Patient(1,1);
+			patientObjEquals.setPatientId(patient.getPatientId());
+			assertTrue(patient.equals(patientObjEquals));
+			assertFalse(patient.equals(null));
+			patientObjEquals.setPatientId(null);
+			assertFalse(patient.equals(patientObjEquals));
+			
 		}
 	 @Test
 	 public void testGetPatientId()
 		{
 		    Patient Test = new Patient(3,3);
 		    UUID f=Test.getPatientId();
-		  
 		}
+	 @Test
+	 public void testSetPatientId()
+		{
+		    Patient Test = new Patient(3,3);
+		    Test.setPatientId(UUID.randomUUID());
+		}
+	
 	@Test
 	public void testAddDisease()
 	{
